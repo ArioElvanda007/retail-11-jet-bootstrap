795a541404c4670c5c9f4afafd5f0b6afba7e254
ArioElvanda007 <arioelvandavbnet@gmail.com>
2024-09-10 17:00:47 +0200

    Create Procedure Report Stock

--- database/migrations/2024_09_08_033721_create_sp_stock_table.php
+++ database/migrations/2024_09_08_033721_create_sp_stock_table.php
@@ -12,12 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        $procedure = "DROP PROCEDURE IF EXISTS spStock;
-        
-        CREATE PROCEDURE spStock (IN spIidx BIGINT, spDate DATE)
+        DB::unprepared('DROP PROCEDURE IF EXISTS spStock;');
+        DB::unprepared("CREATE PROCEDURE spStock (IN spIidx BIGINT, spDate DATE)
         BEGIN
             DROP TEMPORARY TABLE IF EXISTS tempProd;
-            
             IF spIidx = 0 THEN
                 CREATE TEMPORARY TABLE tempProd
                 SELECT * FROM products;
@@ -28,50 +26,258 @@ return new class extends Migration
                 SELECT * FROM products WHERE id = spIidx;
             END IF;
             
+            
+            
+            
+            DROP TEMPORARY TABLE IF EXISTS tempAdj;
+            IF spIidx = 0 THEN
+				CREATE TEMPORARY TABLE tempAdj
+				SELECT 
+					A.prod_id, B.rate, A.date_input
+				FROM
+				(
+					SELECT 
+						MAX(id) AS id, prod_id, date_input 
+					FROM stocks
+					WHERE date_input <= spDate 
+					GROUP BY date_input, prod_id
+				) AS A
+				INNER JOIN
+					stocks AS B ON A.id = B.id
+				;   
+            END IF;
+            
+            IF spIidx <> 0 THEN
+				CREATE TEMPORARY TABLE tempAdj
+				SELECT 
+					A.prod_id, B.rate, A.date_input
+				FROM
+				(
+					SELECT 
+						MAX(id) AS id, prod_id, date_input 
+					FROM stocks
+					WHERE date_input <= spDate
+					GROUP BY date_input, prod_id
+				) AS A
+				INNER JOIN
+					stocks AS B ON A.id = B.id
+				WHERE B.prod_id = spIidx
+				;   
+            END IF;
+
+            
+            DROP TEMPORARY TABLE IF EXISTS tempBuy;
+            IF spIidx = 0 THEN
+				CREATE TEMPORARY TABLE tempBuy
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+				FROM 
+					buying AS A
+				INNER JOIN
+					buying_details AS B ON A.id = B.buying_id WHERE A.date_input = spDate
+					GROUP BY B.prod_id, A.date_input
+				;
+            END IF;
+            
+            IF spIidx <> 0 THEN
+				CREATE TEMPORARY TABLE tempBuy
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+				FROM 
+					buying AS A
+				INNER JOIN
+					buying_details AS B ON A.id = B.buying_id WHERE A.date_input = spDate AND B.prod_id = spIidx
+					GROUP BY B.prod_id, A.date_input
+				;
+            END IF;
+            
+            
+            
+            
+            DROP TEMPORARY TABLE IF EXISTS tempBuyBegin;
+            IF spIidx = 0 THEN
+				CREATE TEMPORARY TABLE tempBuyBegin
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+				FROM 
+					buying AS A
+				INNER JOIN
+					buying_details AS B ON A.id = B.buying_id WHERE A.date_input < spDate
+					GROUP BY B.prod_id
+				; 
+            END IF;
+            
+			IF spIidx <> 0 THEN
+				CREATE TEMPORARY TABLE tempBuyBegin
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+				FROM 
+					buying AS A
+				INNER JOIN
+					buying_details AS B ON A.id = B.buying_id WHERE A.date_input < spDate AND B.prod_id = spIidx
+					GROUP BY B.prod_id
+				; 
+            END IF;
+            
+            
+            
+                    
+                    
+                    
+            DROP TEMPORARY TABLE IF EXISTS tempSell;
+            IF spIidx = 0 THEN
+				CREATE TEMPORARY TABLE tempSell
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+				FROM 
+					selling AS A
+				INNER JOIN
+					selling_details AS B ON A.id = B.selling_id WHERE A.date_input = spDate
+					GROUP BY B.prod_id, A.date_input
+				; 
+			END IF;
+
+            IF spIidx <> 0 THEN
+				CREATE TEMPORARY TABLE tempSell
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+				FROM 
+					selling AS A
+				INNER JOIN
+					selling_details AS B ON A.id = B.selling_id WHERE A.date_input = spDate AND B.prod_id = spIidx 
+					GROUP BY B.prod_id, A.date_input
+				; 
+			END IF;
+            
+            
+            
+            
+            
+            
+            DROP TEMPORARY TABLE IF EXISTS tempSellBegin;
+            IF spIidx = 0 THEN
+				CREATE TEMPORARY TABLE tempSellBegin
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+				FROM 
+					selling AS A
+				INNER JOIN
+					selling_details AS B ON A.id = B.selling_id WHERE A.date_input < spDate
+					GROUP BY B.prod_id
+				;     
+            END IF;
+            
+            IF spIidx <> 0 THEN
+				CREATE TEMPORARY TABLE tempSellBegin
+				SELECT 
+					B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+				FROM 
+					selling AS A
+				INNER JOIN
+					selling_details AS B ON A.id = B.selling_id WHERE A.date_input < spDate AND B.prod_id = spIidx 
+					GROUP BY B.prod_id
+				;     
+            END IF;
+            
+            
+            
+            
+            
+            
+            DROP TEMPORARY TABLE IF EXISTS tempProd2;
+            CREATE TEMPORARY TABLE tempProd2
             SELECT 
-                A.id, A.code, A.name, A.price_buy, A.price_sell,
-                IFNULL(D.rate, 0) + IFNULL(G.rate, 0) - 
-                IFNULL(J.rate, 0) AS stock, A.description, A.created_at, A.updated_at
-            FROM
+                A.id, A.code, A.name, A.price_buy, A.price_sell,     
+                ifnull((SELECT ifnull(rate, 0) FROM tempAdj WHERE A.id = prod_id AND DATE(date_input) = (MAX(CASE WHEN DATE(B.date_input) < DATE(spDate) THEN B.date_input ELSE '1900-01-01' END))), 0) AS adj_begining,
+				ifnull(E.rate, 0) AS buy_begining,
+                ifnull(F.rate, 0) AS sell_begining,
+
+                MAX(CASE WHEN DATE(B.date_input) = DATE(spDate) THEN B.rate ELSE 0 END) AS adj_current,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(spDate) THEN C.rate ELSE 0 END) AS buy_current,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(spDate) THEN D.rate ELSE 0 END) AS sell_current,
+                
+                A.description, A.created_at, A.updated_at
+            FROM 
                 tempProd AS A
             LEFT OUTER JOIN
-                (
-                    SELECT 
-                        B.id, B.prod_id, B.rate 
-                    FROM 
-                        stocks AS B
-                    INNER JOIN
-                        (
-                            SELECT MAX(id) AS id, MAX(date_input) AS date_input FROM stocks
-                            WHERE date_input <= spDate
-                            GROUP BY prod_id    
-                        ) AS C ON B.id = C.id
-                ) AS D ON A.id = D.prod_id
+                tempAdj AS B ON A.id = B.prod_id
             LEFT OUTER JOIN
-                (
-                    SELECT 
-                        F.prod_id, IFNULL(SUM(F.rate), 0) AS rate
-                    FROM 
-                        buying AS E
-                    INNER JOIN
-                        buying_details AS F ON E.id = F.buying_id WHERE E.date_input <= spDate 
-                ) AS G ON A.id = G.prod_id
+                tempBuy AS C ON A.id = C.prod_id
             LEFT OUTER JOIN
-                (
-                    SELECT 
-                        I.prod_id, IFNULL(SUM(I.rate), 0) AS rate
-                    FROM 
-                        selling AS H
-                    INNER JOIN
-                        selling_details AS I ON H.id = I.selling_id WHERE H.date_input <= spDate
-                ) AS J ON A.id = J.prod_id    
-            -- GROUP BY A.id, A.name, A.price_buy, A.price_sell, A.description, A.created_at, A.updated_at
+                tempSell AS D ON A.id = D.prod_id
+            LEFT OUTER JOIN
+                tempBuyBegin AS E ON A.id = E.prod_id    
+            LEFT OUTER JOIN
+                tempSellBegin AS F ON A.id = F.prod_id    
+            GROUP BY A.id, A.name, A.price_buy, A.price_sell, A.description, A.created_at, A.updated_at
             ;
-        END
+            
+            SELECT 
+				id, name, price_buy, price_sell, (adj_begining + buy_begining - sell_begining) + adj_current + buy_current - sell_current AS stock, description, created_at, updated_at
+            FROM tempProd2
+            ;
+        END");
 
-        ;";
+        // $procedure = "DROP PROCEDURE IF EXISTS spStock;
         
-        DB::unprepared($procedure);
+        // CREATE PROCEDURE spStock (IN spIidx BIGINT, spDate DATE)
+        // BEGIN
+        //     DROP TEMPORARY TABLE IF EXISTS tempProd;
+            
+        //     IF spIidx = 0 THEN
+        //         CREATE TEMPORARY TABLE tempProd
+        //         SELECT * FROM products;
+        //     END IF;
+        
+        //     IF spIidx <> 0 THEN
+        //         CREATE TEMPORARY TABLE tempProd
+        //         SELECT * FROM products WHERE id = spIidx;
+        //     END IF;
+            
+        //     SELECT 
+        //         A.id, A.code, A.name, A.price_buy, A.price_sell,
+        //         IFNULL(D.rate, 0) + IFNULL(G.rate, 0) - 
+        //         IFNULL(J.rate, 0) AS stock, A.description, A.created_at, A.updated_at
+        //     FROM
+        //         tempProd AS A
+        //     LEFT OUTER JOIN
+        //         (
+        //             SELECT 
+        //                 B.id, B.prod_id, B.rate 
+        //             FROM 
+        //                 stocks AS B
+        //             INNER JOIN
+        //                 (
+        //                     SELECT MAX(id) AS id, MAX(date_input) AS date_input FROM stocks
+        //                     WHERE date_input <= spDate
+        //                     GROUP BY prod_id    
+        //                 ) AS C ON B.id = C.id
+        //         ) AS D ON A.id = D.prod_id
+        //     LEFT OUTER JOIN
+        //         (
+        //             SELECT 
+        //                 F.prod_id, IFNULL(SUM(F.rate), 0) AS rate
+        //             FROM 
+        //                 buying AS E
+        //             INNER JOIN
+        //                 buying_details AS F ON E.id = F.buying_id WHERE E.date_input <= spDate 
+        //         ) AS G ON A.id = G.prod_id
+        //     LEFT OUTER JOIN
+        //         (
+        //             SELECT 
+        //                 I.prod_id, IFNULL(SUM(I.rate), 0) AS rate
+        //             FROM 
+        //                 selling AS H
+        //             INNER JOIN
+        //                 selling_details AS I ON H.id = I.selling_id WHERE H.date_input <= spDate
+        //         ) AS J ON A.id = J.prod_id    
+        //     -- GROUP BY A.id, A.name, A.price_buy, A.price_sell, A.description, A.created_at, A.updated_at
+        //     ;
+        // END
+
+        // ;";
+        
+        // DB::unprepared($procedure);
 
     }
 
@@ -80,6 +286,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('spStock');
+        DB::unprepared('DROP PROCEDURE IF EXISTS spStock;');
     }
 };
---
+++ database/migrations/2024_09_10_182803_create_sp_report_stock.php
@@ -0,0 +1,209 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::unprepared('DROP PROCEDURE IF EXISTS spReportStock;');
+        DB::unprepared("CREATE PROCEDURE spReportStock (IN fromDate DATE)
+        BEGIN    
+            DECLARE toDate DATE;
+            SET toDate = DATE_ADD(fromDate, INTERVAL 30 DAY);    
+
+            DROP TEMPORARY TABLE IF EXISTS tempAdj;
+            CREATE TEMPORARY TABLE tempAdj
+            SELECT 
+                A.prod_id, B.rate, A.date_input
+            FROM
+            (
+                SELECT 
+                    MAX(id) AS id, prod_id, date_input 
+                FROM stocks
+                WHERE date_input <= toDate 
+                GROUP BY date_input, prod_id
+            ) AS A
+            INNER JOIN
+                stocks AS B ON A.id = B.id
+            ;   
+
+
+            
+            DROP TEMPORARY TABLE IF EXISTS tempBuy;
+            CREATE TEMPORARY TABLE tempBuy
+            SELECT 
+                B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+            FROM 
+                buying AS A
+            INNER JOIN
+                buying_details AS B ON A.id = B.buying_id WHERE A.date_input BETWEEN fromDate AND toDate 
+                GROUP BY B.prod_id, A.date_input
+            ;     
+            
+            
+            DROP TEMPORARY TABLE IF EXISTS tempBuyBegin;
+            CREATE TEMPORARY TABLE tempBuyBegin
+            SELECT 
+                B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+            FROM 
+                buying AS A
+            INNER JOIN
+                buying_details AS B ON A.id = B.buying_id WHERE A.date_input < fromDate
+                GROUP BY B.prod_id
+            ; 
+                    
+            DROP TEMPORARY TABLE IF EXISTS tempSell;
+            CREATE TEMPORARY TABLE tempSell
+            SELECT 
+                B.prod_id, IFNULL(SUM(B.rate), 0) AS rate, A.date_input
+            FROM 
+                selling AS A
+            INNER JOIN
+                selling_details AS B ON A.id = B.selling_id WHERE A.date_input BETWEEN fromDate AND toDate 
+                GROUP BY B.prod_id, A.date_input
+            ; 
+
+            DROP TEMPORARY TABLE IF EXISTS tempSellBegin;
+            CREATE TEMPORARY TABLE tempSellBegin
+            SELECT 
+                B.prod_id, IFNULL(SUM(B.rate), 0) AS rate
+            FROM 
+                selling AS A
+            INNER JOIN
+                selling_details AS B ON A.id = B.selling_id WHERE A.date_input < fromDate
+                GROUP BY B.prod_id
+            ;     
+            
+            SELECT 
+                A.id, A.code, A.name,     
+                ifnull((SELECT ifnull(rate, 0) FROM tempAdj WHERE A.id = prod_id AND DATE(date_input) = (MAX(CASE WHEN DATE(B.date_input) < DATE(fromDate) THEN B.date_input ELSE '1900-01-01' END))), 0) AS adj_date00,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(fromDate) THEN B.rate ELSE 0 END) AS adj_date01,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 1 DAY)) THEN B.rate ELSE 0 END) AS adj_date02,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 2 DAY)) THEN B.rate ELSE 0 END) AS adj_date03,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 3 DAY)) THEN B.rate ELSE 0 END) AS adj_date04,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 4 DAY)) THEN B.rate ELSE 0 END) AS adj_date05,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 5 DAY)) THEN B.rate ELSE 0 END) AS adj_date06,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 6 DAY)) THEN B.rate ELSE 0 END) AS adj_date07,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 7 DAY)) THEN B.rate ELSE 0 END) AS adj_date08,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 8 DAY)) THEN B.rate ELSE 0 END) AS adj_date09,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 9 DAY)) THEN B.rate ELSE 0 END) AS adj_date10,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 10 DAY)) THEN B.rate ELSE 0 END) AS adj_date11,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 11 DAY)) THEN B.rate ELSE 0 END) AS adj_date12,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 12 DAY)) THEN B.rate ELSE 0 END) AS adj_date13,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 13 DAY)) THEN B.rate ELSE 0 END) AS adj_date14,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 14 DAY)) THEN B.rate ELSE 0 END) AS adj_date15,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 15 DAY)) THEN B.rate ELSE 0 END) AS adj_date16,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 16 DAY)) THEN B.rate ELSE 0 END) AS adj_date17,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 17 DAY)) THEN B.rate ELSE 0 END) AS adj_date18,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 18 DAY)) THEN B.rate ELSE 0 END) AS adj_date19,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 19 DAY)) THEN B.rate ELSE 0 END) AS adj_date20,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 20 DAY)) THEN B.rate ELSE 0 END) AS adj_date21,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 21 DAY)) THEN B.rate ELSE 0 END) AS adj_date22,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 22 DAY)) THEN B.rate ELSE 0 END) AS adj_date23,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 23 DAY)) THEN B.rate ELSE 0 END) AS adj_date24,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 24 DAY)) THEN B.rate ELSE 0 END) AS adj_date25,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 25 DAY)) THEN B.rate ELSE 0 END) AS adj_date26,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 26 DAY)) THEN B.rate ELSE 0 END) AS adj_date27,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 27 DAY)) THEN B.rate ELSE 0 END) AS adj_date28,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 28 DAY)) THEN B.rate ELSE 0 END) AS adj_date29,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 29 DAY)) THEN B.rate ELSE 0 END) AS adj_date30,
+                MAX(CASE WHEN DATE(B.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 30 DAY)) THEN B.rate ELSE 0 END) AS adj_date31,
+
+                ifnull(E.rate, 0) AS buy_date00,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(fromDate) THEN C.rate ELSE 0 END) AS buy_date01,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 1 DAY)) THEN C.rate ELSE 0 END) AS buy_date02,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 2 DAY)) THEN C.rate ELSE 0 END) AS buy_date03,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 3 DAY)) THEN C.rate ELSE 0 END) AS buy_date04,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 4 DAY)) THEN C.rate ELSE 0 END) AS buy_date05,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 5 DAY)) THEN C.rate ELSE 0 END) AS buy_date06,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 6 DAY)) THEN C.rate ELSE 0 END) AS buy_date07,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 7 DAY)) THEN C.rate ELSE 0 END) AS buy_date08,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 8 DAY)) THEN C.rate ELSE 0 END) AS buy_date09,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 9 DAY)) THEN C.rate ELSE 0 END) AS buy_date10,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 10 DAY)) THEN C.rate ELSE 0 END) AS buy_date11,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 11 DAY)) THEN C.rate ELSE 0 END) AS buy_date12,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 12 DAY)) THEN C.rate ELSE 0 END) AS buy_date13,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 13 DAY)) THEN C.rate ELSE 0 END) AS buy_date14,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 14 DAY)) THEN C.rate ELSE 0 END) AS buy_date15,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 15 DAY)) THEN C.rate ELSE 0 END) AS buy_date16,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 16 DAY)) THEN C.rate ELSE 0 END) AS buy_date17,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 17 DAY)) THEN C.rate ELSE 0 END) AS buy_date18,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 18 DAY)) THEN C.rate ELSE 0 END) AS buy_date19,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 19 DAY)) THEN C.rate ELSE 0 END) AS buy_date20,       
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 20 DAY)) THEN C.rate ELSE 0 END) AS buy_date21,       
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 21 DAY)) THEN C.rate ELSE 0 END) AS buy_date22,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 22 DAY)) THEN C.rate ELSE 0 END) AS buy_date23,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 23 DAY)) THEN C.rate ELSE 0 END) AS buy_date24,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 24 DAY)) THEN C.rate ELSE 0 END) AS buy_date25,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 25 DAY)) THEN C.rate ELSE 0 END) AS buy_date26,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 26 DAY)) THEN C.rate ELSE 0 END) AS buy_date27,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 27 DAY)) THEN C.rate ELSE 0 END) AS buy_date28,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 28 DAY)) THEN C.rate ELSE 0 END) AS buy_date29,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 29 DAY)) THEN C.rate ELSE 0 END) AS buy_date30,
+                MAX(CASE WHEN DATE(C.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 30 DAY)) THEN C.rate ELSE 0 END) AS buy_date31,
+                
+                ifnull(F.rate, 0) AS sell_date00,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(fromDate) THEN D.rate ELSE 0 END) AS sell_date01,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 1 DAY)) THEN D.rate ELSE 0 END) AS sell_date02,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 2 DAY)) THEN D.rate ELSE 0 END) AS sell_date03,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 3 DAY)) THEN D.rate ELSE 0 END) AS sell_date04,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 4 DAY)) THEN D.rate ELSE 0 END) AS sell_date05,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 5 DAY)) THEN D.rate ELSE 0 END) AS sell_date06,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 6 DAY)) THEN D.rate ELSE 0 END) AS sell_date07,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 7 DAY)) THEN D.rate ELSE 0 END) AS sell_date08,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 8 DAY)) THEN D.rate ELSE 0 END) AS sell_date09,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 9 DAY)) THEN D.rate ELSE 0 END) AS sell_date10,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 10 DAY)) THEN D.rate ELSE 0 END) AS sell_date11,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 11 DAY)) THEN D.rate ELSE 0 END) AS sell_date12,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 12 DAY)) THEN D.rate ELSE 0 END) AS sell_date13,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 13 DAY)) THEN D.rate ELSE 0 END) AS sell_date14,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 14 DAY)) THEN D.rate ELSE 0 END) AS sell_date15,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 15 DAY)) THEN D.rate ELSE 0 END) AS sell_date16,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 16 DAY)) THEN D.rate ELSE 0 END) AS sell_date17,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 17 DAY)) THEN D.rate ELSE 0 END) AS sell_date18,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 18 DAY)) THEN D.rate ELSE 0 END) AS sell_date19,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 19 DAY)) THEN D.rate ELSE 0 END) AS sell_date20,       
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 20 DAY)) THEN D.rate ELSE 0 END) AS sell_date21,       
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 21 DAY)) THEN D.rate ELSE 0 END) AS sell_date22,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 22 DAY)) THEN D.rate ELSE 0 END) AS sell_date23,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 23 DAY)) THEN D.rate ELSE 0 END) AS sell_date24,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 24 DAY)) THEN D.rate ELSE 0 END) AS sell_date25,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 25 DAY)) THEN D.rate ELSE 0 END) AS sell_date26,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 26 DAY)) THEN D.rate ELSE 0 END) AS sell_date27,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 27 DAY)) THEN D.rate ELSE 0 END) AS sell_date28,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 28 DAY)) THEN D.rate ELSE 0 END) AS sell_date29,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 29 DAY)) THEN D.rate ELSE 0 END) AS sell_date30,
+                MAX(CASE WHEN DATE(D.date_input) = DATE(DATE_ADD(fromDate, INTERVAL 30 DAY)) THEN D.rate ELSE 0 END) AS sell_date31
+                
+            FROM 
+                products AS A
+            LEFT OUTER JOIN
+                tempAdj AS B ON A.id = B.prod_id
+            LEFT OUTER JOIN
+                tempBuy AS C ON A.id = C.prod_id
+            LEFT OUTER JOIN
+                tempSell AS D ON A.id = D.prod_id
+            LEFT OUTER JOIN
+                tempBuyBegin AS E ON A.id = E.prod_id    
+            LEFT OUTER JOIN
+                tempSellBegin AS F ON A.id = F.prod_id    
+            GROUP BY A.id, A.code, A.name
+            ;
+        END");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::unprepared('DROP PROCEDURE IF EXISTS spReportStock;');
+    }
+};
