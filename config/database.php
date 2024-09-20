98904c2d298f3eb5d1e4680634666d2420c4cfa0
ArioElvanda007 <arioelvandavbnet@gmail.com>
2024-09-20 13:32:34 +0200

    Set DB_HOST

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'elvh3918'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'elvh3918_diapersky'),
             'username' => env('DB_USERNAME', 'elvh3918_root'),
