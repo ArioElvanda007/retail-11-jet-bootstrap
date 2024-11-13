4bce64885702f2765a99ddfdf3efa102884dc61d
ArioElvanda007 <arioelvandavbnet@gmail.com>
2024-11-13 15:38:32 +0100

    Create access report buying

--- app/Http/Controllers/Report/Buying/ReportBuyingController.php
+++ app/Http/Controllers/Report/Buying/ReportBuyingController.php
@@ -8,8 +8,17 @@ use Carbon\Carbon;
 
 class ReportBuyingController extends Controller
 {
+    private function can_access()
+    {
+        return "App\Http\Controllers\Function\GlobalController";
+    }
+
     public function index()
     {
+        if (app($this->can_access())->access('report buying')->access[0]->modules->is_active == 0 || app($this->can_access())->access('report buying')->access[0]->can_view == 0) {
+            return abort(401);
+        }
+
         $breadcrumbs = [
             ['link' => "dashboard", 'name' => "Dashboard"],
             ['link' => "report.buying.index", 'name' => "Report Buying"]
